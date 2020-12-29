16eb22cb6ab36b9c060a0c0481e766f83f35540f
Brian Hirth <brianhirth404@gmail.com>
2020-12-29 13:23:12 +0100

    Add pinch to zoom

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -4,29 +4,53 @@ import {
   StyleSheet,
   Dimensions,
   Pressable,
+  Animated,
 } from 'react-native';
+
+import { PinchGestureHandler, State } from 'react-native-gesture-handler'
 
 interface PageProps {
   uri: string,
   onPress: () => void,
 };
 
+let scale = new Animated.Value(1);
+
 function Page({ uri, onPress }: PageProps) {
   const { width, height } = Dimensions.get('window');
+
+  const onPinchEvent = Animated.event(
+    [{ nativeEvent: { scale } }],
+    { useNativeDriver: true }
+  );
+
+  const onPinchStateChange = event => {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      Animated.spring(scale, {
+        toValue: 1,
+        useNativeDriver: true
+      }).start()
+    }
+  };
 
   return (
     <Pressable
       onPress={onPress}
       style={styles.container}
     >
-      <Image
-        source={{ uri }}
-        resizeMode='contain'
-        style={{
-          height,
-          width,
-        }}
-      />
+      <PinchGestureHandler
+        onGestureEvent={onPinchEvent}
+        onHandlerStateChange={onPinchStateChange}>
+        <Animated.Image
+          source={{ uri }}
+          resizeMode='contain'
+          style={{
+            height,
+            width,
+            transform: [{ scale }]
+          }}
+        />
+      </PinchGestureHandler>
     </Pressable>
   );
 }
