d2fc4e7e07ad6e248169672542bb98ec558fc402
Brian Hirth <brianhirth404@gmail.com>
2024-05-01 17:46:43 +0200

    Move away from Expo

--- App.tsx
+++ App.tsx
@@ -3,10 +3,10 @@ import React, { useState, useEffect } from "react";
 import { StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import axios from "axios";
-import { useFonts } from "expo-font";
-import { useKeepAwake } from "expo-keep-awake";
+// import { useFonts } from "expo-font";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
@@ -16,14 +16,13 @@ import Loading from "./src/components/Loading";
 import { getChapters, getImages } from "./src/utils";
 
 function App() {
-  useKeepAwake();
 
-  const [loaded] = useFonts({
-    InterMedium: require("./assets/fonts/Inter-Medium.otf"),
-    InterSemiBold: require("./assets/fonts/Inter-SemiBold.otf"),
-    InterBold: require("./assets/fonts/Inter-Bold.otf"),
-    InterBlack: require("./assets/fonts/Inter-Black.otf"),
-  });
+  // const [loaded] = useFonts({
+  //   InterMedium: require("./assets/fonts/Inter-Medium.otf"),
+  //   InterSemiBold: require("./assets/fonts/Inter-SemiBold.otf"),
+  //   InterBold: require("./assets/fonts/Inter-Bold.otf"),
+  //   InterBlack: require("./assets/fonts/Inter-Black.otf"),
+  // });
 
   useEffect(() => {
     StatusBar.setBarStyle("dark-content");
@@ -31,15 +30,17 @@ function App() {
 
   const Stack = createNativeStackNavigator();
 
-  if (!loaded) return null;
+  // if (!loaded) return null;
 
   return (
+    <GestureHandlerRootView>
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="ChapterList" component={ChapterList} />
         <Stack.Screen name="Chapter" component={Chapter} />
       </Stack.Navigator>
     </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
 
